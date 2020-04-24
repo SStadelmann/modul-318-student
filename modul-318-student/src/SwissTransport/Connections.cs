e7fa63cc2cd2ce0999a8b0e389dd1edb6dea9a82
SStadelmann <64000012+SStadelmann@users.noreply.github.com>
2020-04-24 15:54:28 +0200

    Add files via upload

---
+++ modul-318-student/src/SwissTransport/Connections.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SwissTransport
+{
+    public class Connections
+    {
+        [JsonProperty("connections")]
+        public List<Connection> ConnectionList { get; set; } 
+    }
+
+    public class Connection
+    {
+        [JsonProperty("from")]
+        public ConnectionPoint From  { get; set; }
+
+        [JsonProperty("to")]
+        public ConnectionPoint To { get; set; }
+
+        [JsonProperty("duration")]
+        public string Duration { get; set; }
+    }
+
+    public class ConnectionPoint
+    {
+        [JsonProperty("station")]
+        public Station Station { get; set; }
+
+        [JsonProperty("arrival")]
+        public string Arrival { get; set; }
+
+        public string ArrivalTimestamp { get; set; }
+        [JsonProperty("departure")]
+        public string Departure { get; set; }
+
+        public string DepartureTimestamp { get; set; }
+
+        public int? Delay { get; set; }
+
+        public string Platform { get; set; }
+
+        public string RealtimeAvailability { get; set; }
+    }
+}
